96f764bf4e1e534099d46a87d721bd234f1d9a94
Dipak <dipakxb@gmail.com>
2026-03-10 16:32:30 +0530

    Add palindromes function

--- foundations/Js/13_palindromes/palindromes.js
+++ foundations/Js/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const specialCases = "!,. ";
+    for(let i of str){
+        if(specialCases.includes(i)){
+            str = str.slice(0, str.indexOf(i)) + str.slice(str.indexOf(i)+1, str.length);
+        }
+    }
+    let reverseStr = str.split('').reverse().join('');
+    return reverseStr.toLowerCase() === str.toLowerCase();
 };
 
 // Do not edit below this line
