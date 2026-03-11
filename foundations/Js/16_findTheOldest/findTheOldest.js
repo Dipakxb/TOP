4209f5d568afb669a587dd7c73fb56400d0d992a
Dipak <dipakxb@gmail.com>
2026-03-11 18:49:56 +0530

    Add FindTheOldest Alive function

--- foundations/Js/16_findTheOldest/findTheOldest.js
+++ foundations/Js/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let winner = {};
+    for(let i =0; i < arr.length; i++){
+        const thisYear = new Date();
+        let current = arr[i]
+        current.yearOfDeath = current.yearOfDeath ? current.yearOfDeath : thisYear.getFullYear();
+        let currentAge = current.yearOfDeath - current.yearOfBirth;
+        let next = arr[i + 1]
+        if(next === undefined){
+            return winner;
+        }
+        winner = current;
+        next.yearOfDeath = next.yearOfDeath ? next.yearOfDeath : thisYear.getFullYear();
+        let nextAge = next.yearOfDeath - next.yearOfBirth;
+        if(currentAge < nextAge){
+            winner = next;
+        }else {
+            winner = current;
+            break;
+        }
+    }
+    return winner
 };
 
 // Do not edit below this line
