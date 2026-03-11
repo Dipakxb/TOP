e8cb749ac237b374c89c94d5e13137ad6c40fb9e
Dipak <dipakxb@gmail.com>
2026-03-11 17:12:10 +0530

    Add fibonacci sequence function

--- foundations/Js/14_fibonacci/fibonacci.js
+++ foundations/Js/14_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(n < 0){
+        return "OOPS";
+    }
+    let sequence = [1, 1];
+    for(let i = 2; i <= n; i++){
+        sequence[i] = sequence[i - 2] + sequence[i - 1]
+    }
+    return sequence[n-1] ? sequence[n-1] : 0;
 };
 
 // Do not edit below this line
