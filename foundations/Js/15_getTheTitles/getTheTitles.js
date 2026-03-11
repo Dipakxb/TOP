9da0773232a41a35f5e5d1b2b14507dd86e3bd5e
Dipak <dipakxb@gmail.com>
2026-03-11 17:19:56 +0530

    add geTheTitles Function

--- foundations/Js/15_getTheTitles/getTheTitles.js
+++ foundations/Js/15_getTheTitles/getTheTitles.js
@@ -1,4 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    const titleArr = [];
+    for(let obj of arr){
+        titleArr.push(obj.title)
+    }
+    return titleArr;
 
 };
 
